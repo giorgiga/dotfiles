0800a70d0d202a3395dcb50d365d940fa9a5e196
Giorgio Gallo <giorgio.gallo@bitnic.it>
2020-11-28 23:35:50 +0100

    use ls --file-type

--- private_dot_config/private_fish/functions/ls.fish
+++ private_dot_config/private_fish/functions/ls.fish
@@ -1,6 +1,6 @@
 
 function ls --wraps ls --description 'List contents of directory'
     set -l opts --color=auto --group-directories-first
-    isatty stdout; and set -a opts -F -h
+    isatty stdout; and set -a opts -h --file-type
     command ls $opts $argv
 end
